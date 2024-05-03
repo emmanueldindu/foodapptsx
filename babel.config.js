40a3096b20894906e0af17181639a3fd50b0d54e
emmanueldindu <emmanuelnwobodoc04@gmail.com>
2024-05-03 15:00:52 +0100

    map start

--- babel.config.js
+++ babel.config.js
@@ -5,14 +5,6 @@ module.exports = function(api) {
     plugins: [
       "nativewind/babel",
     "react-native-reanimated/plugin",
-    "module:react-native-dotenv", {
-      "envName": "APP_ENV",
-      "moduleName": "@env",
-      "path": ".env",
-      "safe": false,
-      "allowUndefined": true,
-      "verbose": false
-    }
     ],
   };
 };
--- modal/LocationModal.jsx
+++ modal/LocationModal.jsx
@@ -1,12 +1,40 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import React, { useState } from "react";
+import MapView from "react-native-maps";
 
 const LocationModal = () => {
+  const [location, setLocation] = useState({
+    latitude: 51.5078788,
+    longitude: -0.087777,
+    latitudeDelta: 0.02,
+    longitudeDelta: 0.02,
+  });
   return (
-    <View>
-      <Text>Location Modal {process.env.GOGGLE_API_KEY}</Text>
-    </View>
-  )
-}
+    <View style={{ flex: 1 }}>
+      <MapView style={styles.map} region={location} />
+      <View style={styles.absuluteBtn}>
+        <TouchableOpacity style={styles.btn}>
+            <Text style={styles.buttonText}>Done</Text>
 
-export default LocationModal
+        </TouchableOpacity>
+      </View>
+
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  map: {
+    flex: 1,
+  },
+  absuluteBtn: {
+    position: "absolute",
+  },
+  btn: {
+
+  },
+  buttonText: {
+
+  }
+});
+export default LocationModal;
